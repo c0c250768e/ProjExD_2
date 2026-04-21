fbcb4c8e8db434b3d40646f711867de3e26fb706
Murai Kenshin <c0c250768e@edu.teu.ac.jp>
2026-04-21 14:22:50 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -20,6 +21,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = 5, 5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -27,6 +36,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+
+        
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -38,6 +49,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct) 
         pg.display.update()
         tmr += 1
         clock.tick(50)
