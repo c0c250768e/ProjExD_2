cb306b6014fbb8ef506bc58a6f26bb639b8a2fb9
Murai Kenshin <c0c250768e@edu.teu.ac.jp>
2026-04-21 14:45:44 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {
     pg.K_RIGHT: (5, 0),
 }
 
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんrectか、ばくだんrect
+    戻り値：タプル（横方向、縦方向）
+    """
+    
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -48,8 +60,16 @@ def main():
                 sum_mv[1] += mv[1] #練習1
 
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1    
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct) 
         pg.display.update()
         tmr += 1
